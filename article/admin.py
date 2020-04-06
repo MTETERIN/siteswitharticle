cd3ac4e174a078ff676e7aa687e421f29f06dfb2
anamah <mmakhneva@icloud.com>
2020-04-06 21:23:14 +0300

    Database structure has been update Created admin page

--- article/admin.py
+++ article/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import ToolsAndInnovations
+from .models import ToolsAndInnovations, Startdata, Metadata, Recommendations
 # Register your models here.
-admin.site.register(ToolsAndInnovations)
+
+class ToolsAndInnovationAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in ToolsAndInnovations._meta.get_fields()]
+admin.site.register(ToolsAndInnovations, ToolsAndInnovationAdmin)
+
+
+class StartdataAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Startdata._meta.get_fields()]
+admin.site.register(Startdata, StartdataAdmin)
+
+
+class MetadataAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Metadata._meta.get_fields()]
+admin.site.register(Metadata, MetadataAdmin)
+
+
+class RecommendationsAdmin(admin.ModelAdmin):
+    list_display = [f.name for f in Recommendations._meta.get_fields()]
+admin.site.register(Recommendations, RecommendationsAdmin)
